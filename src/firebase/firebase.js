767a9523e8f10828f1d8d62002f0b0b3b91bce43
Prem <theprempratap@gmail.com>
2020-07-30 12:45:55 +0530

    firebase authentication

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -11,7 +11,30 @@ class firebase {
         this.auth = app.auth();
         this.db = app.firestore();
     }
+
+    async register(name, email, password) {
+        const newUser = await this.auth.createUserWithEmailAndPassword(
+            email,
+            password
+        );
+        return newUser.user.updateProfile({
+            displayName: name,
+        });
+    }
+
+    login(email, password) {
+        return this.auth.signInWithEmailAndPassword(email, password);
+    }
+
+    logout() {
+        return this.auth.signOut();
+    }
+
+    resetPassword(email) {
+        return this.auth.sendPasswordResetEmail(email);
+    }
 }
+
 
 const firebase = new firebase();
 export default firebase;
