03752277dc87d0eda103525059dd2da43118e8d5
Prem <theprempratap@gmail.com>
2020-08-04 15:54:10 +0530

    Profile page

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -2,21 +2,21 @@ import React from "react";
 import firebase from "../firebase";
 
 function useAuth() {
-    const [authUser, setAuthUser] = React.useState(null);
+  const [authUser, setAuthUser] = React.useState(null);
 
+  React.useEffect(() => {
+    const unsubscribe = firebase.auth.onAuthStateChanged((user) => {
+      if (user) {
+        setAuthUser(user);
+      } else {
+        setAuthUser(null);
+      }
+    });
 
-    React.userEffect(() =>  {
-        const unsubscribe = firebase.auth.onAuthStateChanged((user) => {
-            if (user) {
-                setAuthUser(user);
-            } else {
-                setAuthUser(null);
-            }
-        });
-        return () => unsubscribe()
-    }, []);
+    return () => unsubscribe();
+  }, []);
 
-    return [authUser, setAuthUser];
+  return [authUser, setAuthUser];
 }
 
 export default useAuth;
